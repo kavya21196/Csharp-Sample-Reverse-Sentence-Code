5e730dfa48942645092ddf2718d3508bc94303c2
kavya21196 <kavya.t@ascendion.com>
2025-12-16 13:17:49 +0530

    even program

---
+++ even.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is Even");
+        }
+    }
+}
