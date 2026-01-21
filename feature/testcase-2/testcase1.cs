317a01d9119e7bf93b098c646ac34221dfe9ddfc
kavya21196 <kavya.t@ascendion.com>
2026-01-21 11:46:05 +0530

    Test File

---
+++ feature/testcase-2/testcase1.cs
@@ -0,0 +1,20 @@
+[
+  {
+    "testcase_id": "PAL-TC-11",
+    "input": "",
+    "expected_result": true,
+    "description": "Empty string is considered a palindrome"
+  },
+  {
+    "testcase_id": "PAL-TC-12",
+    "input": "a",
+    "expected_result": true,
+    "description": "Single character palindrome"
+  },
+  {
+    "testcase_id": "PAL-TC-13",
+    "input": "ab",
+    "expected_result": false,
+    "description": "Two different characters"
+  }
+]
